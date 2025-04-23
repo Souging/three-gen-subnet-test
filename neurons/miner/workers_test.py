e3e24da3c78ddc8eb990afeeab65380cb6a0534a
Souging <150511278+Souging@users.noreply.github.com>
2025-04-23 17:31:19 +0800

    Update workers_test.py

--- neurons/miner/workers_test.py
+++ neurons/miner/workers_test.py
@@ -8,6 +8,7 @@ from pydantic import BaseModel, Field
 from gradio_client import Client, handle_file
 import aiohttp
 import bittensor as bt
+from typing import Optional
 import pyspz
 from openai import OpenAI
 import random
@@ -19,14 +20,7 @@ from common.protocol import PullTask, SubmitResults
 
 from miner import ValidatorSelector
 os.environ["HF_HUB_DISABLE_TELEMETRY"] = "1" 
-class ValidationResponse(BaseModel):
-    score: float = Field(default=0.0, description="Validation score, from 0.0 to 1.0")
-    iqa: float = Field(default=0.0, description="Aesthetic Predictor (quality) score")
-    clip: float = Field(default=0.0, description="Clip similarity score")
-    ssim: float = Field(default=0.0, description="Structure similarity score")
-    lpips: float = Field(default=0.0, description="Perceptive similarity score")
-    preview: str | None = Field(default=None, description="Optional. Preview image, base64 encoded PNG")
-NETWORK_DELAY_TIME_BUFFER = 60
+
 FAILED_VALIDATOR_DELAY = 301
 
 #/tmp/gradio
@@ -53,34 +47,21 @@ async def worker_routine(
         await _complete_one_task(endpoint, wallet, metagraph, validator_selector)
 
 
-def _call_gradio_client(endpoint: str, prompt: str, seed: int) -> str:
-    client = Client(endpoint)
+def _call_gradio_client(endpoint: str, prompt: str) -> str:
     try:
+        client = Client(endpoint)
+        seed=random.randint(0, 2**32 - 1)
         return client.predict(
             prompt=prompt,
-            seed=seed,
-            randomize_seed=True,
-            width=800,
-            height=800,
-            guidance_scale=8,
-            num_inference_steps=12,
-            api_name="/generate_flux_image"
-        )
-    finally:
-        client.close()
-
-def _call_gradio_client_image_to_3d(endpoint: str, image_path: str, seed: int) -> str:
-    client = Client(endpoint)
-    try:
-        return client.predict(
-            image=handle_file(image_path),
             seed=seed,
             ss_guidance_strength=6.5,
             ss_sampling_steps=20,
             slat_guidance_strength=4.0,
             slat_sampling_steps=16,
-            api_name="/image_to_3d"
+            api_name="/text_to_3d"
         )
+    except Exception as e:
+        bt.logging.error(f"生成错误 在 {endpoint} : {e}")
     finally:
         client.close()
 
@@ -121,58 +102,39 @@ async def _complete_one_task(
                 validator_selector.set_cooldown(validator_uid, pull.cooldown_until)
         return
 
-    bt.logging.debug(f"vali_uid :{validator_uid}  获取任务返回. Prompt: {pull.task.prompt}.")
-    cs = 0
-    while True:
-        if cs >= 3:
-            bt.logging.debug(f"vali_uid :{validator_uid} 超过3次低分，跳过 ")
-            results = b'' 
-            break
-        random_seed = random.randint(0, 2**32 - 1)
-        endpoints = random.choice(generate_url)
-        ply_endpoint = ["http://127.0.0.1:10006","http://127.0.0.1:10007"]
+    bt.logging.debug(f"vali_uid :{validator_uid}  获取任务返回 开始多节点生成验证. Prompt: {pull.task.prompt}.")
+    validation_results = []
+    async def generate_ply(endpoint):
         try:
-            client = Client(endpoints)
-            images = await asyncio.to_thread(
+            client = Client(endpoint)
+            random_seed = random.randint(0, 2**32 - 1)
+            ply_path = await asyncio.to_thread(
                 _call_gradio_client,
-                endpoints,
+                endpoint,
                 pull.task.prompt,
                 random_seed,
                 client
             )
-            client = Client(random.choice(ply_endpoint))
-            random_seed = random.randint(0, 2**32 - 1)
-            vresult = await asyncio.to_thread(
-                _call_gradio_client_image_to_3d,
-                endpoints,
-                images,
-                random_seed,
-                client
-            )
+            with open(ply_path, 'rb') as file:
+                ply_bytes = file.read()
+            os.remove(ply_path)
+            compresseds = base64.b64encode(pyspz.compress(ply_bytes, workers=-1)).decode(encoding="utf-8")
+            vail_url = ["http://127.0.0.1:20000", "http://127.0.0.1:20001"]
+            validation_score = await validate(random.choice(vail_url), prompt=pull.task.prompt, results=compresseds, uid=validator_uid)
+            return validation_score,compresseds
         except Exception as e:
-            bt.logging.error(f"Failed to connect to {endpoints}: {str(e)}")
-            continue
-        os.remove(images)
-        results = mp4_to_bytes_open(vresult)
-        os.remove(vresult)
-        compressed_results = base64.b64encode(results).decode(encoding="utf-8")
-        vail_url = ["http://127.0.0.1:20000","http://127.0.0.1:20001"]
-        validation_res = await validate(random.choice(vail_url), prompt=pull.task.prompt,results=compressed_results,uid=validator_uid)
-        cs = cs + 1
-        if validation_res is not None:
-            if validator_uid == 49:
-                if validation_res.score >= 0.69999:
-                    bt.logging.debug(f"vali_uid :{validator_uid} Prompt: {pull.task.prompt} 分数大于0.74 开始提交...")
-                    break
-            else:
-                if validation_res.score >= 0.69999:
-                    bt.logging.debug(f"vali_uid :{validator_uid} Prompt: {pull.task.prompt} 分数大于0.80 开始提交...")
-                    break
-
-    #bt.logging.debug(f"video received. path: {vresult}. len: {len(results)}")
-    
+            bt.logging.error(f"Failed to connect to {endpoint}: {str(e)}")
+            return None, None    
+    tasks = [generate_ply(endpoint) for endpoint in generate_url]
+    validation_results = await asyncio.gather(*tasks)
+    best_score = -1.0
+    best_results = None
+    for validation_score, compresseds in validation_results:
+        if validation_score is not None and validation_score > best_score:
+            best_score = validation_score
+            best_results = compresseds
     async with bt.dendrite(wallet=wallet) as dendrite:
-        submit = await _submit_results(wallet, dendrite, metagraph, validator_uid, pull, results)
+        submit = await _submit_results(wallet, dendrite, metagraph, validator_uid, pull, best_results)
         if submit.feedback is None:
             bt.logging.warning(
                 f"vali_uid :{validator_uid}  提交结果出错 to [{metagraph.hotkeys[validator_uid]}]. "
@@ -180,7 +142,7 @@ async def _complete_one_task(
             )
             validator_selector.set_cooldown(validator_uid, int(time.time()) + FAILED_VALIDATOR_DELAY)
             return 
-    _log_feedback(validator_uid, submit,vresult)
+    _log_feedback(validator_uid, submit)
 
     validator_selector.set_cooldown(validator_uid, submit.cooldown_until)
 
@@ -196,43 +158,41 @@ async def _pull_task(dendrite: bt.dendrite, metagraph: bt.metagraph, validator_u
     return response
 
 async def validate(
-    endpoint: str,prompt: str ,results: str, storage_enabled: bool = False, validation_score_threshold: float = 0.6,uid = None
-) -> ValidationResponse | None:
+    endpoint: str,prompt: str ,results: str, storage_enabled: bool = False, validation_score_threshold: float = 0.6
+) -> Optional[float]:
     prompt = prompt  # type: ignore[union-attr]
     data = results
     validate_url = urllib.parse.urljoin(endpoint, "/validate_txt_to_3d_ply/")
 
-    async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=100)) as session:
+    async with aiohttp.ClientSession() as session:
         try:
             async with session.post(
                 validate_url,
                 json={
                     "prompt": prompt,
                     "data": data,
-                    "compression": 0,
+                    "compression": 2,
                     "generate_preview": storage_enabled,
                     "preview_score_threshold": validation_score_threshold - 0.1,
                 },
             ) as response:
                 if response.status == 200:
                     data_dict = await response.json()
-                    results = ValidationResponse(**data_dict)
-                    bt.logging.debug(f"vuid : {uid} 本地验证分数: {results.score:.2f} | 提示词: {prompt}")
-                    return results
+                    result = data_dict
+                    print(f"Validation score: {result['score']} | Prompt: {prompt}")
+                    return result["score"]
                 else:
-                    bt.logging.debug(f"vuid : {uid} 本地验证错误: [{response.status}] {response.reason}")
+                    print(f"Validation failed: [{response.status}] {response.reason}")
         except aiohttp.ClientConnectorError:
-            bt.logging.warning(f"Failed to connect to the endpoint. The endpoint might be inaccessible: {endpoint}.")
+            print(f"Failed to connect to the endpoint. The endpoint might be inaccessible: {endpoint}.")
         except TimeoutError:
-            bt.logging.warning(f"The request to the endpoint timed out: {endpoint}")
+            print(f"The request to the endpoint timed out: {endpoint}")
         except aiohttp.ClientError as e:
-            bt.logging.warning(f"An unexpected client error occurred: {e} ({endpoint})")
+            print(f"An unexpected client error occurred: {e} ({endpoint})")
         except pydantic.ValidationError as e:
-            bt.logging.warning(f"Incompatible validation response format: {e} ({endpoint})")
+            print(f"Incompatible validation response format: {e} ({endpoint})")
         except Exception as e:
-            bt.logging.warning(f"An unexpected error occurred: {e} ({endpoint})")
-
-    return None
+            print(f"An unexpected error occurred: {e} ({endpoint})")
 async def _submit_results(
     wallet: bt.wallet,
     dendrite: bt.dendrite,
@@ -268,7 +228,7 @@ async def _submit_results(
     return response
 
 
-def _log_feedback(validator_uid: int, submit: SubmitResults,vresult : str) -> None:
+def _log_feedback(validator_uid: int, submit: SubmitResults) -> None:
     feedback = submit.feedback
     if feedback is None:
         return
@@ -279,7 +239,6 @@ def _log_feedback(validator_uid: int, submit: SubmitResults,vresult : str) -> No
         f"平均分数: {feedback.average_fidelity_score}. "
         f"4小时内次数: {feedback.generations_within_the_window}. "
         f"总分数: {feedback.current_miner_reward}."
-        f"path: {vresult}."
         )
     else:
         bt.logging.debug(
